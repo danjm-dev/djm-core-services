2d4d8a343d427861702eee1414c6b43e748018f9
Daniel Miller <danjm.dev@gmail.com>
2023-09-08 11:22:47 +1200

    Added pause and unpause options to application controller, and plugged it into event managerr

---
+++ Runtime/Scripts/ApplicationManagement/GameApplicationController.cs
@@ -0,0 +1,35 @@
+using DJM.CoreUtilities.EventManagement;
+using UnityEngine;
+
+namespace DJM.CoreUtilities
+{
+    public sealed class GameApplicationController : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameEvents.Subscribe<GameApplicationEvents.QuitGameEvent>(Quit);
+            GameEvents.Subscribe<GameApplicationEvents.PauseGameEvent>(Pause);
+            GameEvents.Subscribe<GameApplicationEvents.UnPauseGameEvent>(UnPause);
+        }
+        
+        private void OnDestroy()
+        {
+            GameEvents.Unsubscribe<GameApplicationEvents.QuitGameEvent>(Quit);
+            GameEvents.Unsubscribe<GameApplicationEvents.PauseGameEvent>(Pause);
+            GameEvents.Unsubscribe<GameApplicationEvents.UnPauseGameEvent>(UnPause);
+        }
+
+        private static void Quit(GameApplicationEvents.QuitGameEvent quitGameEventEvent)
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+        
+        private static void Pause(GameApplicationEvents.PauseGameEvent pauseGameEventEvent) => Time.timeScale = 0f;
+        
+        private static void UnPause(GameApplicationEvents.UnPauseGameEvent unpauseGameEventEvent) => Time.timeScale = 1f;
+    }
+}
---
+++ Runtime/Scripts/ApplicationManagement/GameApplicationEvents.cs
@@ -0,0 +1,9 @@
+namespace DJM.CoreUtilities
+{
+    public static class GameApplicationEvents
+    {
+        public readonly struct PauseGameEvent { }
+        public readonly struct UnPauseGameEvent { }
+        public readonly struct QuitGameEvent { }
+    }
+}
